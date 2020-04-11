259cb9a4b7c6920dbd44719ae2fa5ea354d02831
Michael Diente <michael.diente@englishcentral.com>
2020-04-11 16:35:13 +0800

    Working accepter without attributes

--- XMLParser.playground/Contents.swift
+++ XMLParser.playground/Contents.swift
@@ -38,6 +38,68 @@ let sampleInput5 = """
 let sampleInput6 = """
 <magnitude>-5.53</magnitude>
 """
+let sampleInput7 = """
+<?xml version="1.0" encoding="UTF-8"?>
+<WindowElement xmins="http://windiws.lbl.gov" xsi="http://www.w3.org/2001/XMLSchema-instance" schemaLocation="http://windows.lbl.gov BSDG-v1.4xsd">
+<Optical>
+<Layer>
+    <Material>
+        <Name>Perfect Diffuser</Name>
+        <Manufacturer>ACME Surfaces</Manufacturer>
+        <Width unit="Meter">1.000</Width>
+        <Height unit="Meter">1.000</Height>
+    </Material>
+    <DataDefinition>
+        <IncidentDataStructure>TensorTree3</IncidentDataStructure>
+    </DataDefinition>
+    <WavelengthData>
+        <LayerNumber>System</LayerNumber>
+        <Wavelength unit="Integral">Visible</Wavelength>
+        <SourceSpectrum>CIE Illuminant D65 1nm.ssp</SourceSpectrum>
+        <DetectorSpectrum>ASTM E308 1931 Y.dsp</DetectorSpectrum>
+        <WavelengthDataBlock>
+            <WavelengthDataDirection>Reflection Back</WavelengthDataDirection>
+            <AngleBasis>LBNL/Shirley-Chiu</AngleBasis>
+            <ScatteringDataType>BRDF</ScatteringDataType>
+            <ScatteringData>[ 0.318309886 ]</ScatteringData>
+        </WavelengthDataBlock>
+    </WavelengthData>
+</Layer>
+</Optical>
+</WindowElement>
+"""
+let sampleInput8 = """
+<amulet>
+    <gem>sapphire</gem>
+</amulet>
+<DataDefinition>
+    <IncidentDataStructure>TensorTree3</IncidentDataStructure>
+</DataDefinition>
+"""
+
+class Token {
+    var value: String = ""
+    var type: TokenType = .empty
+    
+    init(value: String, type: TokenType) {
+        self.value = value
+        self.type = type
+    }
+}
+
+enum TokenType: String {
+    case undefined
+    case lessThanSymbol
+    case greaterThanSymbol
+    case tag
+    case slash
+    case value
+    case attrib
+    case attribValue
+    case questionMark
+    case oper
+    case empty
+}
 
 extension String.Element {
     var isXmlSymbol: Bool {
@@ -54,8 +116,8 @@ extension String.Element {
 }
 
 
-func getXmlTokens(xmlString: String) -> [String] {
-    var tokens: [String] = []
+func getXmlTokens(xmlString: String) -> [Token] {
+    var tokens: [Token] = []
     let formattedXmlString = xmlString.components(separatedBy: CharacterSet.newlines)
     for lineString in formattedXmlString {
         let formattedLineString = lineString.trimmingCharacters(in: CharacterSet.whitespaces)
@@ -67,16 +129,44 @@ func getXmlTokens(xmlString: String) -> [String] {
             let isValueSymbol = character.isValueSymbol
             let isLetter = character.isLetter
             let isNumber = character.isNumber
+            var type: TokenType = .undefined
             
             if !isLetter && !isQuotes && !isNumber && !isValueSymbol {
                 if !tempString.isEmpty {
-                    tokens.append(tempString)
+                    let lastToken = tokens.last
+                    
+                    switch lastToken?.type {
+                    case .lessThanSymbol:
+                        type = .tag
+                    case .tag:
+                        type = .attrib
+                    case .oper:
+                        type = .attribValue
+                    case .slash:
+                        type = .tag
+                    default:
+                        type = .undefined
+                    }
+                    
+                    tokens.append(Token(value: tempString, type: type))
                     tempString = ""
                 }
             }
             
             if isXmlSymbol {
-                tokens.append(String(character))
+                if character == "<" {
+                    type = .lessThanSymbol
+                } else if character == ">" {
+                    type = .greaterThanSymbol
+                } else if character == "/" {
+                    type = .slash
+                } else if character == "?" {
+                    type = .questionMark
+                } else if character == "=" {
+                    type = .oper
+                }
+                
+                tokens.append(Token(value: String(character), type: type))
             } else {
                 if isLetter || isQuotes || isNumber || isValueSymbol {
                     tempString += String(character)
@@ -84,44 +174,90 @@ func getXmlTokens(xmlString: String) -> [String] {
             }
         }
     }
-    print("getXmlTokens: \(tokens)")
     return tokens
 }
 
 func isXmlFileValid(xmlString: String) {
-    var stack = Array<String>(repeating: "", count: 100)
-    var top = -1
     var tokens = getXmlTokens(xmlString: xmlString)
-    var isValid = false
+    var stack = Array<Token>(repeating: Token(value: "", type: .undefined), count: 100000)
+    var tempToken = Token(value: "", type: .undefined)
+    var top = -1
     
     top += 1
-    stack[top] = "#"
+    stack[top] = Token(value: "", type: .empty)
     
     for token in tokens {
-        if token == "<" || token == ">" || token == "/" || token == "?" {
-            top += 1
-            stack[top] = token
+        // print("token: \(token.value): \(token.type)")
+        print("stack: \(stack[0...top].map { $0.value }), top: \(top)")
+        
+        if stack[top].type == .lessThanSymbol {
+            if token.type == .tag || token.type == .slash {
+                stack[top] = token
+            }
+        } else if stack[top].type == .tag {
+            if token.type == .greaterThanSymbol {
+                tempToken = stack[top]
+                
+                stack[top] = Token(value: "", type: .undefined)
+                top -= 1
+                
+                if stack[top].value == tempToken.value {
+                    stack[top] = Token(value: "", type: .undefined)
+                    top -= 1
+                } else {
+                    // push back
+                    top += 1
+                    stack[top] = tempToken
+                    
+                    top += 1
+                    stack[top] = token
+                }
+            }
+        } else if stack[top].type == .slash {
+            if token.type == .tag {
+                stack[top] = Token(value: "", type: .undefined)
+                top -= 1
+                
+                print("slash -> stack: \(stack[top].value), token: \(token.value)")
+                if stack[top].value == token.value {
+                    stack[top] = Token(value: "", type: .undefined)
+                    top -= 1
+                } else {
+                    top += 1
+                    stack[top] = token
+                }
+            }
+        } else if stack[top].type == .greaterThanSymbol {
+            if token.type == .lessThanSymbol {
+                stack[top] = token
+            }
         } else {
-            if stack[top] == "<" || stack[top] == "/" {
+            if token.type == .greaterThanSymbol {
+                if stack[top].type != .empty {
+                    top += 1
+                    stack[top] = token
+                }
+            } else {
                 top += 1
                 stack[top] = token
             }
         }
     }
     
-    print("top: \(top)")
-    var start = -1
-    while start < top {
-        start += 1
-        print(stack[start], terminator: "")
+    print("stack: \(stack[0...top].map { $0.type }), top: \(top)")
+    if stack[top].type == .empty {
+        stack[top] = Token(value: "", type: .undefined)
+        top -= 1
     }
     
-    if isValid {
+    print("top: \(top)")
+    
+    if top == -1 {
         print("YES")
     } else {
         print("NO")
     }
 }
 
-isXmlFileValid(xmlString: sampleInput1)
+isXmlFileValid(xmlString: sampleInput3)
 
